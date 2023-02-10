6cf7642978dc6e7289ef96990f7030ef0e83d986
joaolotjr <joaolotjr@gmail.com>
2023-02-10 15:40:02 -0300

    Project ends

--- initial-studies/desafio01-calculadora/src/components/Button/index.js
+++ initial-studies/desafio01-calculadora/src/components/Button/index.js
@@ -1,8 +1,10 @@
 import { ButtonContainer } from './styles'
 
 const Button = ({label, onClick}) => {
+
+  console.log(onClick)
     return (
-      <ButtonContainer onClick={onClick}>
+      <ButtonContainer onClick={onClick} type="button">
           {label}
       </ButtonContainer>
     );
